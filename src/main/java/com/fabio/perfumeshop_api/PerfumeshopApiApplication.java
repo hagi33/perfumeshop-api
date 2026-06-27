0b45a28e13b0c17c366e28621d8a01249fe21166
hagi33 <fabiorook9000@gmail.com>
2026-06-27 16:14:51 +0200

    feat: Primer commit del proyecto. Esqueleto del proyecto

---
+++ src/main/java/com/fabio/perfumeshop_api/PerfumeshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.fabio.perfumeshop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumeshopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabio/perfumeshop_api/PerfumeshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio.perfumeshop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumeshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
