7597584f72c3ffadde5e2cb18f8357b22bfd3ddf
hagi33 <fabiorook9000@gmail.com>
2026-07-10 13:32:44 +0200

    feat: Método para controlar excepciones no esperadas

--- src/main/java/com/fabio/perfumeshop_api/shared/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fabio/perfumeshop_api/shared/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.fabio.perfumeshop_api.shared.exception;
 
 import com.fabio.perfumeshop_api.catalog.api.InsufficientStockException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice//Esta anotación hace que la clase capte todas las excepciones producidad
+@Slf4j
+@RestControllerAdvice//Esta anotación hace que la clase capte todas las excepciones producidas por las clases controller del monolito
 public class GlobalExceptionHandler {
 
 
@@ -41,6 +43,18 @@ public class GlobalExceptionHandler {
                     errors.put(e.getField(), e.getDefaultMessage()));
             problem.setProperty("errors", errors);
             return problem;
+        }
+
+
+        //Método que controla excepeciones no esperadas
+        @ExceptionHandler(Exception.class)
+        ProblemDetail handleGeneric(Exception ex){
+        log.error("Error no controlado en la API", ex);
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(
+          HttpStatus.INTERNAL_SERVER_ERROR, "Ha ocurrido un error inesperado"
+        );
+        problem.setTitle("Error interno");
+        return problem;
         }
 
 
